8d226e69f2d525ce50891ed9e68bf9254964f6d2
Liz James <rjames93@sucs.org>
2017-05-10 09:47:31 +0100

    Clean up.
    Still not working correctly... Weekend job

--- travis/common.sh
+++ travis/common.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-function verify_image()
-{
+function verify_image(){
   # track the exit code for this platform
   local exit_code=0
   local build_stdout
@@ -14,31 +12,6 @@ function verify_image()
   echo $sketch
 
   build_stdout=$(arduino -v --verbose-build --verify $sketch 2>&1)
-
-  # echo output if the build failed
-  if [ $? -ne 0 ]; then
-    # heavy X
-    echo -e "\xe2\x9c\x96"
-    echo -e "----------------------------- DEBUG OUTPUT -----------------------------\n"
-    echo "$build_stdout"
-    echo -e "\n------------------------------------------------------------------------\n"
-
-    # mark as fail
-    exit_code=1
-  else
-    # heavy checkmark
-    echo -e "\xe2\x9c\x93"
-  fi
-  # done
-  return $exit_code
-}
-
-function build_image()
-{
-  # track the exit code for this platform
-  local exit_code=0
-  local build_stdout
-  build_stdout=$(arduino --verify $sketch 2>&1)
 
   # echo output if the build failed
   if [ $? -ne 0 ]; then
