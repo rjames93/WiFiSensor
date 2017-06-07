7bb17ae2a199906000d130e215a08e0a56795639
Michael <michael@abbotta>
2017-06-07 18:24:38 +0100

    fix some brackets

--- src/WiFiSensor/WiFiSensor.ino
+++ src/WiFiSensor/WiFiSensor.ino
@@ -147,28 +147,27 @@ void loop() {
       if ( timer <= 100 ) {
         Serial.println("Gathering Data");
         // We need to report to the MQTT Server
-        if(mqttconnect() != 0){
+        if (mqttconnect() != 0) {
           Serial.println("Unable to connect to MQTT Server");
-        }else{
-    			// Connect to MQTT Server
-    			//client.connect(mqttclientname.c_str(), mqttusername.c_str(), mqttpassword.c_str() );
-    			//Following is original
-    			String topicname("sensors/");
-    			topicname += String(sensorname);
-    			topicname += String("/");
-    			mqtttemperaturetopic = "temperature";
-    			mqtthumiditytopic = "humidity";
-    			snprintf(msg, 25, "%s", String(lastmeasurement.temperature).c_str());
-         mqttpublish((topicname + mqtttemperaturetopic), msg);
-    			snprintf(msg, 75, "%s", String(lastmeasurement.humidity).c_str());
+        } else {
+          // Connect to MQTT Server
+          //client.connect(mqttclientname.c_str(), mqttusername.c_str(), mqttpassword.c_str() );
+          //Following is original
+          String topicname("sensors/");
+          topicname += String(sensorname);
+          topicname += String("/");
+          mqtttemperaturetopic = "temperature";
+          mqtthumiditytopic = "humidity";
+          snprintf(msg, 25, "%s", String(lastmeasurement.temperature).c_str());
+          mqttpublish((topicname + mqtttemperaturetopic), msg);
+          snprintf(msg, 75, "%s", String(lastmeasurement.humidity).c_str());
           mqttpublish((topicname + mqtthumiditytopic), msg);
-			}		
-			
-			
-			}
-		}
-        
+        }
+
+
+      }
     }
-    }
+
   }
 }
+
