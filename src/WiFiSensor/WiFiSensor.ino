a8844466850cb492e91a0c615c0318ecb7ff4281
Michael <michael@abbotta>
2017-06-07 16:44:21 +0100

    bRought in code for MQTT Config Introduced code from m

--- src/WiFiSensor/WiFiSensor.ino
+++ src/WiFiSensor/WiFiSensor.ino
@@ -147,11 +147,16 @@ void loop() {
       if ( timer <= 100 ) {
         Serial.println("Gathering Data");
         // We need to report to the MQTT Server
-        if(!mqttconnect()){
+        if(mqttconnect() != 0){
           Serial.print("Unable to connect to MQTT Server");
         }else{
-			// Connected to MQTT Server
+			// Connect to MQTT Server
+			//Code in brought in from mqttconnect()
+			mqttclientname = root["mqtt-settings"]["client_name"].asString();
+			mqttusername = root["mqtt-settings"]["username"].asString();
+			mqttpassword = root["mqtt-settings"]["password"].asString();
 			client.connect(mqttclientname.c_str(), mqttusername.c_str(), mqttpassword.c_str() );
+			//Following is original
 			String topicname("sensors/");
 			topicname += String(sensorname);
 			topicname += String("/");
