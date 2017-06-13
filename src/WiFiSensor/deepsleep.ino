d925b747931423119012a957fc4c2f0e5267221a
Mike Abbott <michael@abbottastro.com>
2017-06-13 13:46:17 +0100

    Removed if timer in deepsleep

--- src/WiFiSensor/deepsleep.ino
+++ src/WiFiSensor/deepsleep.ino
@@ -9,31 +9,31 @@ void deepsleep() {
 
   if (mqttmode == true) {
 	  Serial.println("Handling MQTT");
-	  if ( timer <= 100 ) {
-		Serial.println("Gathering Data");
-		// We need to report to the MQTT Server
-		if (mqttconnect() != 0) {
-		  Serial.println("Unable to connect to MQTT Server");
-		} else {
-		  // Connect to MQTT Server
-		  //client.connect(mqttclientname.c_str(), mqttusername.c_str(), mqttpassword.c_str() );
-		  //Following is original
-		  String topicname("sensors/");
-		  topicname += String(sensorname);
-		  topicname += String("/");
-		  /*
-		  //Manual Variable Setting
-		  mqtttemperaturetopic = "temperature";
-		  mqtthumiditytopic = "humidity";
-		  */
-		  snprintf(msg, 25, "%s", String(lastmeasurement.temperature).c_str());
-		  mqttpublish((topicname + mqtttemperaturetopic), msg);
-		  snprintf(msg, 75, "%s", String(lastmeasurement.humidity).c_str());
-		  mqttpublish((topicname + mqtthumiditytopic), msg);
-		}
+	  
+	Serial.println("Gathering Data");
+	// We need to report to the MQTT Server
+	if (mqttconnect() != 0) {
+	  Serial.println("Unable to connect to MQTT Server");
+	} else {
+	  // Connect to MQTT Server
+	  //client.connect(mqttclientname.c_str(), mqttusername.c_str(), mqttpassword.c_str() );
+	  //Following is original
+	  String topicname("sensors/");
+	  topicname += String(sensorname);
+	  topicname += String("/");
+	  /*
+	  //Manual Variable Setting
+	  mqtttemperaturetopic = "temperature";
+	  mqtthumiditytopic = "humidity";
+	  */
+	  snprintf(msg, 25, "%s", String(lastmeasurement.temperature).c_str());
+	  mqttpublish((topicname + mqtttemperaturetopic), msg);
+	  snprintf(msg, 75, "%s", String(lastmeasurement.humidity).c_str());
+	  mqttpublish((topicname + mqtthumiditytopic), msg);
+	}
 
 
-	  }
+	  
   }
 
 
